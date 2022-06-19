37e91a4a10855faeaae1bc4c5bb4b48404a5f88a
Elucide <yoseph.schecroun@gmail.c>
2022-06-19 21:34:49 +0200

    added header

--- push_swap.h
+++ push_swap.h
@@ -12,6 +12,7 @@ typedef struct s_data
 	int	len_b;
 }	t_data;
 
+void	print_stacks(t_data *data);
 int ft_atoi(char *str);
 int	ft_split_int(char *str, t_data *data);
 int	fill_a(int nb, t_data *data);
@@ -24,5 +25,13 @@ int	swap_b(t_data *data);
 int	swap_s(t_data *data);
 int	push_a(t_data *data);
 int	push_b(t_data *data);
-int	clean_stack(int	*stack, int len);
-int	add_stack(int *stack, int len, int nb);
+int	rot_a(t_data *data);
+int	rot_b(t_data *data);
+int	rrot_a(t_data *data);
+int	rrot_b(t_data *data);
+int	rr(t_data *data);
+int	rrr(t_data *data);
+int	*stack_del_front(int *stack, int len);
+int	*stack_add_front(int *stack, int len, int nb);
+int	*stack_del_back(int *stack, int len);
+int	*stack_add_back(int *stack, int len, int nb);
